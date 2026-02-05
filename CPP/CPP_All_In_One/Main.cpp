15a6e8201b0130573005c343d6e2904e4bd6988d
MinSeok0406 <hen0406@naver.com>
2026-02-05 17:31:18 +0900

    시스템 프로그래밍 핸들 테이블
    26.02.05

--- CPP/CPP_All_In_One/Main.cpp
+++ CPP/CPP_All_In_One/Main.cpp
@@ -17,7 +17,117 @@ using namespace std;
 
 #pragma comment(lib, "ws2_32.lib")
 
+class StandardNewAndDelete
+{
+public:
+    // 기본 버전 new
+    static void* operator new(size_t size) noexcept
+    {
+        try
+        {
+            // 예외 발생 처리
 
+            return ::operator new(size);
+        }
+        catch (const bad_alloc& e)
+        {
+        }
+    }
+
+    // 기본 버전 delete
+    static void operator delete(void* pMemory) noexcept
+    {
+        try
+        {
+            // 예외 발생 처리
+
+            ::operator delete(pMemory);
+        }
+        catch (const exception& e)
+        {
+        }
+    }
+
+    // 위치지정 new
+    static void* operator new(size_t size, void* ptr) noexcept
+    {
+        try
+        {
+            // 예외 발생 처리
+
+            return ::operator new(size, ptr);
+        }
+        catch (const exception& e)
+        {
+        }
+    }
+
+    // 위치지정 delete
+    static void operator delete(void* pMemory, void* ptr) noexcept
+    {
+        try
+        {
+            // 예외 발생 처리
+
+            ::operator delete(pMemory, ptr);
+        }
+        catch (const exception& e)
+        {
+        }
+    }
+
+    // 위치지정 new
+    static void* operator new(size_t size, const nothrow_t& nt) noexcept
+    {
+        try
+        {
+            // 예외 발생 처리
+
+            return ::operator new(size, nt);
+        }
+        catch (const exception& e)
+        {
+        }
+    }
+
+    // 위치지정 delete
+    static void operator delete(void* pMemory, const nothrow_t& t) noexcept
+    {
+        try
+        {
+            // 예외 발생 처리
+
+            ::operator delete(pMemory);
+        }
+        catch (const exception& e)
+        {
+        }
+    }
+};
+
+
+class Widget : public StandardNewAndDelete
+{
+public:
+    using StandardNewAndDelete::operator new;
+    using StandardNewAndDelete::operator delete;
+
+    static void* operator new(size_t size, ostream& logStream)
+    {
+        cout << "위치지정 new" << "\n";
+        return 0;
+    }
+
+    static void operator delete(void* pMemory, size_t size) noexcept
+    {
+        cout << "기본형 delete" << "\n";
+    }
+
+    static void operator delete(void* pMemory, ostream& logStream) noexcept
+    {
+        cout << "위치지정 delete" << "\n";
+    }
+};
 
 int main()
 {
---
+++ Windows System Programming/Brain_Windows System Programming/MailSender2_2.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <locale.h>
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <tchar.h>
+
+
+int _tmain(int argc, TCHAR* argv[])
+{
+    _wsetlocale(LC_ALL, L"korean");
+
+    HANDLE hMailSlot;
+    TCHAR message[50];
+    DWORD bytesWritten;
+
+    // 핸들을 얻는 코드
+    FILE* file = _tfopen(_T("InheritableHandle.txt"), _T("rt"));
+    _ftscanf(file, _T("%p"), &hMailSlot);
+    fclose(file);
+    _tprintf(_T("Inheritable Handle : %p \n"), hMailSlot);
+
+    while (1)
+    {
+        _fputts(_T("MY CMD>"), stdout);
+        _fgetts(message, sizeof(message) / sizeof(TCHAR), stdin);
+
+        if (!WriteFile(hMailSlot, message, _tcslen(message) * sizeof(TCHAR), &bytesWritten, NULL))
+        {
+            _fputts(_T("Unable to write!"), stdout);
+            _gettchar();
+            CloseHandle(hMailSlot);
+            return 1;
+        }
+
+        if (!_tcscmp(message, _T("exit")))
+        {
+            _fputts(_T("Good Bye!"), stdout);
+            break;
+        }
+    }
+
+    CloseHandle(hMailSlot);
+
+    return 0;
+}
