312a78ac201bdb8b42f399ff3f0a5341a52e9f30
MinSeok0406 <hen0406@naver.com>
2026-02-08 16:05:32 +0900

    시스템 프로그래밍 8장 프로젝트 완료
    26.02.08

--- Windows System Programming/Command_Prompt_Practice/MailSender2_1.c
+++ Windows System Programming/Command_Prompt_Practice/MailSender2_1.c
@@ -1,76 +1,76 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <locale.h>
-#include <stdio.h>
-#include <string.h>
-#include <windows.h>
-#include <tchar.h>
-
-#define SLOT_NAME _T("\\\\.\\mailslot\\mailbox")
-
-int _tmain(int argc, TCHAR* argv[])
-{
-    _wsetlocale(LC_ALL, L"korean");
-
-    HANDLE hMailSolt;
-    TCHAR message[50];
-    DWORD bytesWritten;
-
-    SECURITY_ATTRIBUTES sa;
-    sa.nLength = sizeof(sa);
-    sa.lpSecurityDescriptor = NULL;
-    sa.bInheritHandle = TRUE;
-
-    hMailSolt = CreateFile(
-        SLOT_NAME,
-        GENERIC_WRITE,
-        FILE_SHARE_READ,
-        &sa,
-        OPEN_EXISTING,
-        FILE_ATTRIBUTE_NORMAL,
-        NULL
-    );
-
-    if (hMailSolt == INVALID_HANDLE_VALUE)
-    {
-        _fputts(_T("Unable to create mailslot!"), stdout);
-        return 1;
-    }
-
-    _tprintf(_T("Inheritable Handle : %p \n"), hMailSolt);
-
-    // 메일슬롯과 연결한 핸들 정보 메모장에 저장
-    TCHAR handleStr[64];
-    _stprintf_s(handleStr, _countof(handleStr), _T("%p"), hMailSolt);
-    SetEnvironmentVariable(_T("Handle"), handleStr);
-
-    STARTUPINFO si = {0, };
-    PROCESS_INFORMATION pi;
-    si.cb = sizeof(si);
-    TCHAR command[] = _T("ProcessTemp.exe");
-
-    CreateProcess(NULL, command, NULL, NULL, TRUE,
-        CREATE_NEW_CONSOLE | CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi);
-
-    while (1)
-    {
-        _fputts(_T("MY CMD>"), stdout);
-        _fgetts(message, sizeof(message) / sizeof(TCHAR), stdin);
-
-        if (!WriteFile(hMailSolt, message, _tcslen(message) * sizeof(TCHAR), &bytesWritten, NULL))
-        {
-            _fputts(_T("Unable to write!"), stdout);
-            CloseHandle(hMailSolt);
-            return 1;
-        }
-
-        if (!_tcscmp(message, _T("exit")))
-        {
-            _fputts(_T("Good Bye!"), stdout);
-            break;
-        }
-    }
-
-    CloseHandle(hMailSolt);
-
-    return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <locale.h>
+//#include <stdio.h>
+//#include <string.h>
+//#include <windows.h>
+//#include <tchar.h>
+//
+//#define SLOT_NAME _T("\\\\.\\mailslot\\mailbox")
+//
+//int _tmain(int argc, TCHAR* argv[])
+//{
+//    _wsetlocale(LC_ALL, L"korean");
+//
+//    HANDLE hMailSolt;
+//    TCHAR message[50];
+//    DWORD bytesWritten;
+//
+//    SECURITY_ATTRIBUTES sa;
+//    sa.nLength = sizeof(sa);
+//    sa.lpSecurityDescriptor = NULL;
+//    sa.bInheritHandle = TRUE;
+//
+//    hMailSolt = CreateFile(
+//        SLOT_NAME,
+//        GENERIC_WRITE,
+//        FILE_SHARE_READ,
+//        &sa,
+//        OPEN_EXISTING,
+//        FILE_ATTRIBUTE_NORMAL,
+//        NULL
+//    );
+//
+//    if (hMailSolt == INVALID_HANDLE_VALUE)
+//    {
+//        _fputts(_T("Unable to create mailslot!"), stdout);
+//        return 1;
+//    }
+//
+//    _tprintf(_T("Inheritable Handle : %p \n"), hMailSolt);
+//
+//    // 메일슬롯과 연결한 핸들 정보 환경변수에 저장
+//    TCHAR handleStr[64];
+//    _stprintf_s(handleStr, _countof(handleStr), _T("%p"), hMailSolt);
+//    SetEnvironmentVariable(_T("Handle"), handleStr);
+//
+//    STARTUPINFO si = {0, };
+//    PROCESS_INFORMATION pi;
+//    si.cb = sizeof(si);
+//    TCHAR command[] = _T("ProcessTemp.exe");
+//
+//    CreateProcess(NULL, command, NULL, NULL, TRUE,
+//        CREATE_NEW_CONSOLE | CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi);
+//
+//    while (1)
+//    {
+//        _fputts(_T("MY CMD>"), stdout);
+//        _fgetts(message, sizeof(message) / sizeof(TCHAR), stdin);
+//
+//        if (!WriteFile(hMailSolt, message, _tcslen(message) * sizeof(TCHAR), &bytesWritten, NULL))
+//        {
+//            _fputts(_T("Unable to write!"), stdout);
+//            CloseHandle(hMailSolt);
+//            return 1;
+//        }
+//
+//        if (!_tcscmp(message, _T("exit")))
+//        {
+//            _fputts(_T("Good Bye!"), stdout);
+//            break;
+//        }
+//    }
+//
+//    CloseHandle(hMailSolt);
+//
+//    return 0;
+//}
--- Windows System Programming/Command_Prompt_Practice/Program.c
+++ Windows System Programming/Command_Prompt_Practice/Program.c
@@ -1,170 +1,273 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <tchar.h>
-//#include <locale.h>
-//#include <windows.h>
-//
-//#define STR_LEN 256
-//#define CMD_TOKEN_NUM 10
-//
-//TCHAR ERROR_CMD[] = _T("'%s'은(는) 실행할 수 있는 프로그램이 아닙니다. \n");
-//TCHAR cmdString[STR_LEN];
-//TCHAR cmdTokenList[CMD_TOKEN_NUM][STR_LEN];
-//TCHAR seps[] = _T(" ,\t\n");
-//
-//int CmdReadTokenize(void);
-//int CmdProcessing(int);
-//TCHAR* StrLower(TCHAR*);
-//
-//int _tmain(int argc, TCHAR* argv[])
-//{
-//    _tsetlocale(LC_ALL, L"korean");
-//
-//    if (argc > 2)
-//    {
-//        for (int i = 1; i < argc; i++)
-//        {
-//            _tcscpy(cmdTokenList[i - 1], argv[i]);
-//        }
-//
-//        CmdProcessing(argc - 1);
-//    }
-//
-//    DWORD isExit = NULL;
-//    while (1)
-//    {
-//        int tokenNum = CmdReadTokenize();
-//
-//        if (tokenNum == 0)
-//        {
-//            continue;
-//        }
-//
-//        isExit = CmdProcessing(tokenNum);
-//        if (isExit == TRUE)
-//        {
-//            _fputts(_T("명령어 처리를 종료합니다. \n"), stdout);
-//            break;
-//        }
-//    }
-//
-//    return 0;
-//}
-//
-//int CmdReadTokenize(void)
-//{
-//    TCHAR* token;
-//
-//    _fputts(_T("Best command prompt>> "), stdout);
-//    _getts_s(cmdString, sizeof(cmdString));
-//
-//    token = _tcstok(cmdString, seps);
-//    int tokenNum = 0;
-//
-//    while (token != NULL)
-//    {
-//        _tcscpy(cmdTokenList[tokenNum++], StrLower(token));
-//        token = _tcstok(NULL, seps);
-//    }
-//
-//    return tokenNum;
-//}
-//
-//int CmdProcessing(int tokenNum)
-//{
-//    BOOL isRun;
-//    STARTUPINFO si = {0, };
-//    PROCESS_INFORMATION pi;
-//
-//    TCHAR cmdStringWithOptions[STR_LEN] = {0, };
-//    TCHAR optString[STR_LEN] = {0, };
-//
-//    si.cb = sizeof(si);
-//    if (!_tcscmp(cmdTokenList[0], _T("exit")))
-//    {
-//        return TRUE;
-//    }
-//    else if (!_tcscmp(cmdTokenList[0], _T("notepad")))
-//    {
-//        TCHAR command[] = _T("notepad.exe");
-//        SetCurrentDirectory(_T("C:\\WINDOWS\\system32"));
-//
-//        ZeroMemory(&pi, sizeof(pi));
-//        CreateProcess(NULL, command, NULL, NULL,
-//            TRUE, 0, NULL, NULL, &si, &pi);
-//
-//        CloseHandle(pi.hProcess);
-//        CloseHandle(pi.hThread);
-//    }
-//    else if (!_tcscmp(cmdTokenList[0], _T("start")))
-//    {
-//        if (tokenNum > 1)
-//        {
-//            // cmd 형태로 프로세스를 생성 시에는 /k 혹은 /c 을 붙여야 문자열이 버퍼에 저장된다
-//            _tcscat(optString, _T("/k"));
-//            for (int i = 1; i < tokenNum; i++)
-//            {
-//                _stprintf(optString, _T("%s %s"), optString, cmdTokenList[i]);
-//                _stprintf(cmdStringWithOptions, _T("%s %s"), _T("cmd.exe"), optString);
-//            }
-//        }
-//        else
-//        {
-//            _stprintf(cmdStringWithOptions, _T("%s"), _T("cmd.exe"));
-//        }
-//
-//        isRun = CreateProcess(NULL, cmdStringWithOptions, NULL, NULL,
-//            TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
-//
-//        CloseHandle(pi.hProcess);
-//        CloseHandle(pi.hThread);
-//    }
-//    else if (!_tcscmp(cmdTokenList[0], _T("echo")))
-//    {
-//        for (int i = 1; i < tokenNum; i++)
-//        {
-//            _stprintf(optString, _T("%s %s"), optString, cmdTokenList[i]);
-//        }
-//
-//        _tprintf(_T("echo message: %s \n"), optString);
-//    }
-//    else
-//    {
-//        _tcscpy(cmdStringWithOptions, cmdTokenList[0]);
-//
-//        for (int i = 1; i < tokenNum; i++)
-//        {
-//            _stprintf(cmdStringWithOptions, _T("%s %s"), cmdStringWithOptions, cmdTokenList[i]);
-//        }
-//
-//        isRun = CreateProcess(NULL, cmdStringWithOptions, NULL, NULL,
-//            TRUE, 0, NULL, NULL, &si, &pi);
-//
-//        CloseHandle(pi.hProcess);
-//        CloseHandle(pi.hThread);
-//    
-//        if (isRun == FALSE)
-//        {
-//            _tprintf(ERROR_CMD, cmdTokenList[0]);
-//        }
-//    }
-//
-//    return 0;
-//}
-//
-//TCHAR* StrLower(TCHAR* pStr)
-//{
-//    TCHAR* ret = pStr;
-//
-//    while (*pStr)
-//    {
-//        if (_istupper(*pStr))
-//        {
-//            *pStr = _totlower(*pStr);
-//        }
-//        pStr++;
-//    }
-//
-//    return ret;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <tchar.h>
+#include <locale.h>
+#include <windows.h>
+#include <TlHelp32.h>
+
+#define STR_LEN 256
+#define CMD_TOKEN_NUM 10
+
+TCHAR ERROR_CMD[] = _T("'%s'은(는) 실행할 수 있는 프로그램이 아닙니다. \n");
+TCHAR cmdString[STR_LEN];
+TCHAR cmdTokenList[CMD_TOKEN_NUM][STR_LEN];
+TCHAR seps[] = _T(" ,\t\n");
+
+int CmdReadTokenize(void);
+int CmdProcessing(int);
+TCHAR* StrLower(TCHAR*);
+
+int _tmain(int argc, TCHAR* argv[])
+{
+    _tsetlocale(LC_ALL, L"korean");
+
+    if (argc > 2)
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            _tcscpy(cmdTokenList[i - 1], argv[i]);
+        }
+
+        CmdProcessing(argc - 1);
+    }
+
+    DWORD isExit = NULL;
+    while (1)
+    {
+        int tokenNum = CmdReadTokenize();
+
+        if (tokenNum == 0)
+        {
+            continue;
+        }
+
+        isExit = CmdProcessing(tokenNum);
+        if (isExit == TRUE)
+        {
+            _fputts(_T("명령어 처리를 종료합니다. \n"), stdout);
+            break;
+        }
+    }
+
+    return 0;
+}
+
+int CmdReadTokenize(void)
+{
+    TCHAR* token;
+
+    _fputts(_T("Best command prompt>> "), stdout);
+    _getts_s(cmdString, sizeof(cmdString));
+
+    token = _tcstok(cmdString, seps);
+    int tokenNum = 0;
+
+    while (token != NULL)
+    {
+        _tcscpy(cmdTokenList[tokenNum++], StrLower(token));
+        token = _tcstok(NULL, seps);
+    }
+
+    return tokenNum;
+}
+
+int CmdProcessing(int tokenNum)
+{
+    BOOL isRun;
+    STARTUPINFO si = {0, };
+    PROCESS_INFORMATION pi;
+
+    TCHAR cmdStringWithOptions[STR_LEN] = {0, };
+    TCHAR optString[STR_LEN] = {0, };
+
+    si.cb = sizeof(si);
+    if (!_tcscmp(cmdTokenList[0], _T("exit")))
+    {
+        return TRUE;
+    }
+    else if (!_tcscmp(cmdTokenList[0], _T("notepad")))
+    {
+        TCHAR command[] = _T("notepad.exe");
+        SetCurrentDirectory(_T("C:\\WINDOWS\\system32"));
+
+        ZeroMemory(&pi, sizeof(pi));
+        CreateProcess(NULL, command, NULL, NULL,
+            TRUE, 0, NULL, NULL, &si, &pi);
+
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    }
+    else if (!_tcscmp(cmdTokenList[0], _T("start")))
+    {
+        if (tokenNum > 1)
+        {
+            // cmd 형태로 프로세스를 생성 시에는 /k 혹은 /c 을 붙여야 문자열이 버퍼에 저장된다
+            _tcscat(optString, _T("/k"));
+            for (int i = 1; i < tokenNum; i++)
+            {
+                _stprintf(optString, _T("%s %s"), optString, cmdTokenList[i]);
+                _stprintf(cmdStringWithOptions, _T("%s %s"), _T("cmd.exe"), optString);
+            }
+        }
+        else
+        {
+            _stprintf(cmdStringWithOptions, _T("%s"), _T("cmd.exe"));
+        }
+
+        isRun = CreateProcess(NULL, cmdStringWithOptions, NULL, NULL,
+            TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    }
+    else if (!_tcscmp(cmdTokenList[0], _T("echo")))
+    {
+        for (int i = 1; i < tokenNum; i++)
+        {
+            _stprintf(optString, _T("%s %s"), optString, cmdTokenList[i]);
+        }
+
+        _tprintf(_T("echo message: %s \n"), optString);
+    }
+    else if (!_tcscmp(cmdTokenList[0], _T("lp")))
+    {
+        HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+        if (hProcessSnap == INVALID_HANDLE_VALUE)
+        {
+            _tprintf(_T("CreateToolhelp32Snapshot error! \n"));
+            return -1;
+        }
+
+        PROCESSENTRY32 pe32;
+        pe32.dwSize = sizeof(PROCESSENTRY32);
+
+        if (!Process32First(hProcessSnap, &pe32))
+        {
+            _tprintf(_T("Process32First error! \n"));
+            CloseHandle(hProcessSnap);
+            return -1;
+        }
+
+        do
+        {
+            _tprintf(_T("%25s %5d \n"), pe32.szExeFile, pe32.th32ProcessID);
+        }
+        while (Process32Next(hProcessSnap, &pe32));
+
+        CloseHandle(hProcessSnap);
+    }
+    else if (!_tcscmp(cmdTokenList[0], _T("kp")))
+    {
+        HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+        if (hProcessSnap == INVALID_HANDLE_VALUE)
+        {
+            _tprintf(_T("CreateToolhelp32Snapshot error! \n"));
+            return -1;
+        }
+
+        PROCESSENTRY32 pe32;
+        pe32.dwSize = sizeof(PROCESSENTRY32);
+
+        if (!Process32First(hProcessSnap, &pe32))
+        {
+            _tprintf(_T("Process32First error! \n"));
+            CloseHandle(hProcessSnap);
+            return -1;
+        }
+
+        // cmdTokenList[1] 값만 써서 사용해도 됨
+        // 이 반복문은 없어도 됨
+        _stprintf(optString, _T("%s %s"), optString, cmdTokenList[1]);
+        for (int i = 0; i < _tcslen(optString); i++)
+        {
+            if (optString[i] == '\0')
+            {
+                break;
+            }
+            optString[i] = optString[i + 1];
+        }
+
+        TCHAR* command;
+        do
+        {
+            command = StrLower(pe32.szExeFile);
+            if (!_tcscmp(command, optString))
+            {
+                break;
+            }
+        } while (Process32Next(hProcessSnap, &pe32));
+
+        if (_tcscmp(command, optString))
+        {
+            _tprintf(_T("command name error! \n"));
+            CloseHandle(hProcessSnap);
+            return -1;
+        }
+
+        HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
+        CloseHandle(process);
+
+        if (process == NULL)
+        {
+            _tprintf(_T("OpenProcess error! \n"));
+            CloseHandle(hProcessSnap);
+            return -1;
+        }
+
+        DWORD return_val;
+        if (GetExitCodeProcess(process, &return_val) == 0)
+        {
+            _tprintf(_T("GetExitCodeProcess error! \n"));
+            CloseHandle(hProcessSnap);
+            return -1;
+        }
+
+        if (TerminateProcess(process, return_val) == 0)
+        {
+            _tprintf(_T("TerminateProcess error! \n"));
+            CloseHandle(hProcessSnap);
+            return -1;
+        }
+
+        CloseHandle(hProcessSnap);
+    }
+    else
+    {
+        _tcscpy(cmdStringWithOptions, cmdTokenList[0]);
+
+        for (int i = 1; i < tokenNum; i++)
+        {
+            _stprintf(cmdStringWithOptions, _T("%s %s"), cmdStringWithOptions, cmdTokenList[i]);
+        }
+
+        isRun = CreateProcess(NULL, cmdStringWithOptions, NULL, NULL,
+            TRUE, 0, NULL, NULL, &si, &pi);
+
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    
+        if (isRun == FALSE)
+        {
+            _tprintf(ERROR_CMD, cmdTokenList[0]);
+        }
+    }
+
+    return 0;
+}
+
+TCHAR* StrLower(TCHAR* pStr)
+{
+    TCHAR* ret = pStr;
+
+    while (*pStr)
+    {
+        if (_istupper(*pStr))
+        {
+            *pStr = _totlower(*pStr);
+        }
+        pStr++;
+    }
+
+    return ret;
+}
