a9b7ad83ec17497de52066f095f9031fe8b5aba7
MinSeok0406 <145419718+MinSeok0406@users.noreply.github.com>
2026-06-05 20:57:24 +0900

    정리
    2026.06.05

--- CPP/CPP_All_In_One/Main.cpp
+++ CPP/CPP_All_In_One/Main.cpp
@@ -32,30 +32,18 @@ using ll = long long;
 #define tclog std::clog
 #endif
 
-TCHAR str[100];
-HANDLE hEvent;
-HANDLE hMutex;
-
-unsigned int WINAPI outputThreadFunction(LPVOID lpParam)
+class Widget
 {
-    WaitForSingleObject(hEvent, INFINITE);
-    WaitForSingleObject(hMutex, INFINITE);
+public:
+    Widget() { cout << "Widget" << "\n"; }
+    ~Widget() { cout << "~Widget" << "\n"; }
+}; 
 
-    tcout << "Output string : " << str << "\n";
-
-    ReleaseMutex(hMutex);
-
-    return 0;
-}
-
-unsigned int WINAPI countThreadFunction(LPVOID lpParam)
+unsigned int WINAPI threadProc(LPVOID lpParam)
 {
-    WaitForSingleObject(hEvent, INFINITE);
-    WaitForSingleObject(hMutex, INFINITE);
+    unique_ptr<Widget> uptr = make_unique<Widget>();
 
-    tcout << "output string length : " << _tcslen(str) - 1 << "\n";
 
-    ReleaseMutex(hMutex);
     return 0;
 }
 
@@ -63,26 +51,17 @@ int _tmain(int argc, TCHAR* argv[])
 {
     timeBeginPeriod(1);
     system("chcp 65001");
+
+    HANDLE hThread;
+    DWORD dwThread;
+
+    hThread = (HANDLE)_beginthreadex(NULL, 0, threadProc, NULL, 0, (unsigned*)&dwThread);
+
+    WaitForSingleObject(hThread, INFINITE);
+
+    CloseHandle(hThread);
+
     
-    HANDLE hThread[2];
-    DWORD dwThreadID[2];
 
-    hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-    hMutex = CreateMutex(NULL, FALSE, NULL);
-
-    hThread[0] = (HANDLE)_beginthreadex(NULL, 0, outputThreadFunction, NULL, 0, (unsigned*)&dwThreadID[0]);
-    hThread[1] = (HANDLE)_beginthreadex(NULL, 0, countThreadFunction, NULL, 0, (unsigned*)&dwThreadID[1]);
-
-    _fputts(_T("Insert string : "), stdout);
-    _fgetts(str, 30, stdin);
-
-    SetEvent(hEvent);
-
-    WaitForMultipleObjects(2, hThread, TRUE, INFINITE);
-    CloseHandle(hThread[0]);
-    CloseHandle(hThread[1]);
-    CloseHandle(hEvent);
-    CloseHandle(hMutex);
-    
     return 0;
 }
