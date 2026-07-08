7b4be5b2b0e754b503814e81b3f2619277f4247e
MinSeok0406 <hen0406@naver.com>
2026-07-08 09:35:04 +0900

    가상함수 공부
    26.07.08

--- CPP/CPP_All_In_One/Main.cpp
+++ CPP/CPP_All_In_One/Main.cpp
@@ -4,10 +4,32 @@
 #include <process.h>
 #include <Windows.h>
 #include <algorithm>
+#include <vector>
+#include <string>
 using namespace std;
 using ll = long long;
 
 #pragma comment(lib, "Winmm.lib")
+
+class Base
+{
+public:
+    virtual ~Base() { }
+    virtual void someMethod() { cout << "Base version" << "\n"; }
+    
+protected:
+    int _protectedInt = 0;
+
+private:
+    int _privateInt = 0;
+};
+
+class Derived : public Base
+{
+public:
+    void someMethod() override { cout << "Derived version" << "\n"; }
+    void someOtherMethod() { }
+};
 
 int wmain(int argc, WCHAR* argv[])
 {
