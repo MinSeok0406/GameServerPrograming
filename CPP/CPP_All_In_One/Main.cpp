77c6051683171c40a3213aacf9d5d07677f8d715
MinSeok0406 <145419718+MinSeok0406@users.noreply.github.com>
2026-07-08 15:02:48 +0900

    네트워크 프로그래밍 추가
    2026.07.08

--- CPP/CPP_All_In_One/Main.cpp
+++ CPP/CPP_All_In_One/Main.cpp
@@ -3,36 +3,16 @@
 #include <tchar.h>
 #include <process.h>
 #include <Windows.h>
-#include <algorithm>
-#include <vector>
-#include <string>
 using namespace std;
 using ll = long long;
 
 #pragma comment(lib, "Winmm.lib")
 
-class Base
-{
-public:
-    virtual ~Base() { }
-    virtual void someMethod() { cout << "Base version" << "\n"; }
-    
-protected:
-    int _protectedInt = 0;
-
-private:
-    int _privateInt = 0;
-};
-
-class Derived : public Base
-{
-public:
-    void someMethod() override { cout << "Derived version" << "\n"; }
-    void someOtherMethod() { }
-};
-
 int wmain(int argc, WCHAR* argv[])
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     timeBeginPeriod(1);
 
     
---
+++ Network Programming/Window_SocketProgramming/Main.cpp
@@ -0,0 +1,51 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <tchar.h>
+#include <process.h>
+#include <Windows.h>
+using namespace std;
+using ll = long long;
+
+#pragma comment(lib, "Winmm.lib")
+
+void err_quit(const WCHAR* msg)
+{
+    LPVOID lpMsgBuf;
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL, WSAGetLastError(),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf, 0, NULL);
+    MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+    LocalFree(lpMsgBuf);
+    exit(1);
+}
+
+int wmain(int argc, WCHAR* argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    timeBeginPeriod(1);
+
+    WSAData wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        return 1;
+    }
+
+    MessageBox(NULL, L"Init Success", L"Notice", MB_OK);
+
+    SOCKET tcp_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (tcp_sock == INVALID_SOCKET)
+    {
+        err_quit(L"socket()");
+    }
+
+    MessageBox(NULL, L"TCP Success", L"Notice", MB_OK);
+
+    closesocket(tcp_sock);
+
+    WSACleanup();
+    return 0;
+}
