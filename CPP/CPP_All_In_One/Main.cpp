46970c54648ccbfb8b7182cff54c867b6a38e200
MinSeok0406 <hen0406@naver.com>
2026-07-14 17:45:32 +0900

    멀티스레드 TCP 송수신
    26.07.14

--- CPP/CPP_All_In_One/Main.cpp
+++ CPP/CPP_All_In_One/Main.cpp
@@ -7,12 +7,49 @@
 #include <map>
 #include <string>
 #include <stack>
+#include <queue>
 using namespace std;
 using ll = long long;
 
 #pragma comment(lib, "Winmm.lib")
 
+const int dy[4] = { 0, 1, 0, -1 };
+const int dx[4] = { 1, 0, -1, 0 };
+int t, n, m, k, cnt;
+int adj[54][54];
+int visited[54][54];
 
+void Bfs(int y, int x)
+{
+    visited[y][x] = 1;
+    queue<pair<int, int>> q;
+    q.push({ y, x });
+
+    while (q.empty() == false)
+    {
+        tie(y, x) = q.front();
+        q.pop();
+
+        for (auto i = 0; i < 4; ++i)
+        {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (ny < 0 || ny >= n || nx < 0 || nx >= m || adj[ny][nx] == 0)
+            {
+                continue;
+            }
+
+            if (visited[ny][nx])
+            {
+                continue;
+            }
+
+            visited[ny][nx] = visited[y][x] + 1;
+            q.push({ ny, nx });
+        }
+    }
+}
 
 int wmain(int argc, WCHAR* argv[])
 {
@@ -21,7 +58,43 @@ int wmain(int argc, WCHAR* argv[])
     cout.tie(NULL);
     timeBeginPeriod(1);
 
-    
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> m >> n >> k;
+
+        cnt = 0;
+        for (auto i = 0; i < 54; ++i)
+        {
+            for (auto j = 0; j < 54; ++j)
+            {
+                adj[i][j] = 0;
+                visited[i][j] = 0;
+            }
+        }
+
+        for (auto i = 0; i < k; ++i)
+        {
+            int x, y;
+            cin >> x >> y;
+            adj[y][x] = 1;
+        }
+
+        for (auto i = 0; i < n; ++i)
+        {
+            for (auto j = 0; j < m; ++j)
+            {
+                if (visited[i][j] == 0 && adj[i][j] == 1)
+                {
+                    Bfs(i, j);
+                    cnt++;
+                }
+            }
+        }
+
+        cout << cnt << "\n";
+    }
 
     return 0;
 }
--- Network Programming/Server/Main.cpp
+++ Network Programming/Server/Main.cpp
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <fcntl.h>
 #include <io.h>
+#include <process.h>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <Windows.h>
@@ -16,6 +17,48 @@ using ll = long long;
 
 #define SERVERPORT  47000
 #define BUFSIZE     512
+
+unsigned int WINAPI processClient(LPVOID arg)
+{
+    SOCKET client_sock = (SOCKET)arg;
+    int retval;
+    SOCKADDR_IN clientaddr;
+    wchar_t addr[INET_ADDRSTRLEN];
+    int addrlen;
+    wchar_t buf[BUFSIZE + 1];
+
+    addrlen = sizeof(clientaddr);
+    getpeername(client_sock, (SOCKADDR*)&clientaddr, &addrlen);
+    InetNtop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+
+    while (true)
+    {
+        retval = recv(client_sock, (char*)buf, BUFSIZE, 0);
+        if (retval == SOCKET_ERROR)
+        {
+            wprintf(L"%d\n", WSAGetLastError());
+            break;
+        }
+        else if (retval == 0)
+        {
+            break;
+        }
+
+        buf[retval / sizeof(wchar_t)] = L'\0';
+        wprintf(L"[TCP/%s:%d] %s\n", addr, ntohs(clientaddr.sin_port), buf);
+
+        retval = send(client_sock, (char*)buf, retval, 0);
+        if (retval == SOCKET_ERROR)
+        {
+            wprintf(L"%d\n", WSAGetLastError());
+            break;
+        }
+    }
+
+    closesocket(client_sock);
+    wprintf(L"[TCP 서버] 클라이언트 종료: IP 주소 = %s, 포트 번호 = %d\n", addr, ntohs(clientaddr.sin_port));
+    return 0;
+}
 
 int wmain(int argc, WCHAR* argv[])
 {
@@ -31,9 +74,10 @@ int wmain(int argc, WCHAR* argv[])
         return 1;
     }
 
-    SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (listen_sock == INVALID_SOCKET)
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET)
     {
+        wprintf(L"%d\n", WSAGetLastError());
         return 1;
     }
 
@@ -42,31 +86,32 @@ int wmain(int argc, WCHAR* argv[])
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_port = htons(SERVERPORT);
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    int retval = bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+    int retval = bind(sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
     if (retval == SOCKET_ERROR)
     {
+        wprintf(L"%d\n", WSAGetLastError());
         return 1;
     }
 
-    retval = listen(listen_sock, SOMAXCONN);
+    retval = listen(sock, SOMAXCONN);
     if (retval == SOCKET_ERROR)
     {
+        wprintf(L"%d\n", WSAGetLastError());
         return 1;
     }
 
     SOCKET client_sock;
     SOCKADDR_IN clientaddr;
     int addrlen;
-    int len;
-    wchar_t buf[BUFSIZE + 1];
+    HANDLE hThread;
 
     while (true)
     {
         addrlen = sizeof(clientaddr);
-        client_sock = accept(listen_sock, (sockaddr*)&clientaddr, &addrlen);
-        if (client_sock == INVALID_SOCKET)
+        client_sock = accept(sock, (SOCKADDR*)&clientaddr, &addrlen);
+        if (client_sock == SOCKET_ERROR)
         {
+            wprintf(L"%d\n", WSAGetLastError());
             break;
         }
 
@@ -74,72 +119,18 @@ int wmain(int argc, WCHAR* argv[])
         InetNtop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
         wprintf(L"\n[TCP 서버] 클라이언트 접속 : IP 주소 = %s, 포트 번호 = %d\n", addr, ntohs(clientaddr.sin_port));
 
-        while (true)
+        hThread = (HANDLE*)_beginthreadex(NULL, 0, processClient, (LPVOID)client_sock, 0, NULL);
+        if (hThread == NULL)
         {
-            retval = recv(client_sock, (char*)&len, sizeof(int), MSG_WAITALL);
-            if (retval == SOCKET_ERROR)
-            {
-                wprintf(L"%d\n", WSAGetLastError());
-                break;
-            }
-            else if (retval == 0)
-            {
-                break;
-            }
-
-            retval = recv(client_sock, (char*)buf, len, MSG_WAITALL);
-            if (retval == SOCKET_ERROR)
-            {
-                wprintf(L"%d\n", WSAGetLastError());
-                break;
-            }
-            else if (retval == 0)
-            {
-                break;
-            }
-
-            buf[retval / sizeof(wchar_t)] = L'\0';
-            wprintf(L"[TCP/%s:%d] %s\n", addr, ntohs(clientaddr.sin_port), buf);
-
-            wchar_t sendMessage[BUFSIZE];
-            int sendlen;
-            if (fgetws(sendMessage, BUFSIZE, stdin) == NULL)
-            {
-                break;
-            }
-
-            int length = static_cast<int>(wcslen(sendMessage));
-            if (sendMessage[length - 1] == '\n')
-            {
-                sendMessage[length - 1] = '\0';
-            }
-
-            if (wcslen(sendMessage) == 0)
-            {
-                break;
-            }
-
-            sendlen = static_cast<int>(wcslen(sendMessage) * sizeof(wchar_t));
-            retval = send(client_sock, (char*)&sendlen, sizeof(int), 0);
-            if (retval == SOCKET_ERROR)
-            {
-                wprintf(L"%d\n", WSAGetLastError());
-                break;
-            }
-            
-            retval = send(client_sock, (char*)sendMessage, sendlen, 0);
-            if (retval == SOCKET_ERROR)
-            {
-                wprintf(L"%d\n", WSAGetLastError());
-                break;
-            }
+            closesocket(client_sock);
         }
-
-        closesocket(client_sock);
-        wprintf(L"[TCP 서버] 클라이언트 종료 : IP 주소 = %s, 포트 번호 = %d\n", addr, ntohs(clientaddr.sin_port));
+        else
+        {
+            CloseHandle(hThread);
+        }
     }
 
-    closesocket(listen_sock);
+    closesocket(sock);
     WSACleanup();
 
     return 0;
