6d7a8f4aaced4a6c13de146af66e6ea5d7d5439a
MinSeok0406 <145419718+MinSeok0406@users.noreply.github.com>
2026-08-14 14:45:01 +0900

    레드블랙트리 구현 완료
    26.08.14

--- DataStructure_Algorithm/C++ 알고리즘/RedBlackTree/RedBlackTree.h
+++ DataStructure_Algorithm/C++ 알고리즘/RedBlackTree/RedBlackTree.h
@@ -25,9 +25,9 @@ RBTNode* RBT_SearchNode(RBTNode* tree, ElementType target);
 RBTNode* RBT_SearchMinNode(RBTNode* tree);
 void RBT_InsertNode(RBTNode** tree, RBTNode* newNode);
 void RBT_InsertNodeHelper(RBTNode** tree, RBTNode* newNode);
-RBTNode* RBT_RemoveNode(RBTNode** root, ElementType target);
-void RBT_RebuildAfterInsert(RBTNode** tree, RBTNode* newNode);
-void RBT_RebuildAfterRemove(RBTNode** root, RBTNode* x);
+RBTNode* RBT_RemoveNode(RBTNode** root, ElementType data);
+void RBT_RebuildAfterInsert(RBTNode** root, RBTNode* x);
+void RBT_RebuildAfterRemove(RBTNode** root, RBTNode* successor);
 
 void RBT_PrintTree(RBTNode* node, int depth, int blackCount);
 void RBT_RotateLeft(RBTNode** root, RBTNode* parent);
